e669dc3ae9bc05ef16cf33217c7ead29771d2b6b
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-08-22 18:28:47 -0400

    Create users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# American Board of Allergy and Immunology
+# American Board of Anesthesiology
+# American Board of Colon and Rectal Surgery
+# American Board of Dermatology
+# American Board of Emergency Medicine
+# American Board of Family Medicine
+# American Board of Internal Medicine
+# American Board of Medical Genetics and Genomics
+# American Board of Neurological Surgery
+# American Board of Nuclear Medicine
+# American Board of Obstetrics and Gynecology
+# American Board of Opthalmology
+# American Board of Orthopaedic Surgery
+# American Board of Otolaryngology
+# American Board of Pathology
+# American Board of Pediatrics
+# American Board of Physical Medicine and Rehabilitation
+# American Board of Plastic Surgery
+# American Board of Preventive Medicine
+# American Board of Psychiatry and Neurology
+# American Board of Radiology
+# American Board of Surgery
+# American Board of Thoracic Surgery
+# American Board of Urology

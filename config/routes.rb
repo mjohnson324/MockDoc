53a317239704952954e404d58b2f53f0f07384d6
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-08-24 20:46:37 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resource :session, only: [:create, :destroy]
     resources :users, only: [:show, :create, :update]
+
+    resources :doctors, only: [:show, :index]
+
+    resources :specialties, only: [:index]
+    resources :certifications, only: [:index]
+
+    resources :appointments, only: [:show, :index, :update]
+    resources :reviews, only: [:index, :create]
   end
 
 end

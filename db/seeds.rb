430553c874011dc798a5e593c2ac01d41827407b
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-08-30 14:30:26 -0400

    Seed database more robustly with 110 doctors each having 300 appointments scheduled over the next three weeks

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,33 +26,77 @@ end
 Doctor.destroy_all
 doc_first_names = ["Julius", "Nick", "Jane", "Carl", "Donna",
                    "Matt", "Edith", "Eric", "Michelle", "Joe"]
-
 doc_last_names = ["Hibbert", "Riviera", "Doe", "Johnson", "Edwards",
                   "Miller", "Spencer", "Baez", "Rodriguez", "Schmoe"]
-
 education = ["Johns Hopkins University School of Medicine",
              "Hollywood Upstairs Medical College",
              "Harvard Medical School",
              "Harvard Medical School",
-             "Duke University School of Medicine",
+             "Duke School of Medicine",
              "Washington University School of Medicine",
              "Mayo Medical School",
-             "Vanderbilt University School of Medicine",
+             "Vanderbilt School of Medicine",
              "Yale University School of Medicine",
              "Johns Hopkins University School of Medicine"]
-
 degrees = ["MD", "DO", "MD", "DMD", "MD",
            "MD", "DMD", "MD", "DPM", "MD"]
-
 genders = ["male", "male", "female", "male", "female",
            "male", "female", "male", "female", "male"]
-
 addresses = ["51 W 51st St, New York, NY", "51 W 42nd St, New York, NY",
              "51 W 37th St, New York, NY", "51 W 32nd St, New York, NY",
              "51 W 27th St, New York, NY", "51 W 47th St, New York, NY",
              "51 W 56th St, New York, NY", "51 W 61st St, New York, NY",
              "51 W 63st St, New York, NY", "51 W 22nd St, New York, NY"]
 doctors = []
+
+random_first_name = ["Sharie", "Eric", "Leonardo", "Katina", "German",
+                     "Narcisa", "Lorilee", "Phylicia", "Michael", "Derek",
+                     "Edward", "Erick", "James", "Stephen", "Jeffrey",
+                     "Jose", "Orlando", "Joseph", "Lyndsey", "Raisa",
+                     "Althea", "Melony", "Ezequiel", "Keren", "Lizeth",
+                     "Lacey", "Vivienne", "Jeniffer", "Misti", "Darnell",
+                     "Lise", "Tyler", "Matthew", "Thomas", "Brendan",
+                     "David", "Victor", "Candice", "Enriqueta", "Celine",
+                     "Taunya", "Crystal", "Travis", "Dean", "Leilani",
+                     "Cammie", "Thi", "Sylvia", "Lindsay", "Heide",
+                     "Alan", "Lucas", "Pedro", "Danyelle", "Jennette",
+                     "Dave", "Chastity", "Tameika", "Mindy", "Tesha",
+                     "Codi", "Kaye", "Ping", "Alexa", "Jewell"]
+random_last_name = ["Smith", "Jones", "Taylor", "Williams", "Brown",
+                    "Davies", "Evans", "Wilson", "Thomas", "Roberts",
+                    "Johnson", "Lewis", "Walker", "Robinson", "Wood",
+                    "Thompson", "White", "Watson", "Jackson", "Wright",
+                    "Green", "Harris", "Cooper", "King", "Lee",
+                    "Martin", "Clarke", "James", "Morgan", "Hughes",
+                    "Edwards", "Hill", "Moore", "Clark", "Harrison",
+                    "Scott", "Young", "Morris", "Hall", "Ward",
+                    "Turner", "Carter", "Phillips", "Mitchell", "Patel",
+                    "Adams", "Campbell", "Anderson"]
+random_gender = ["male", "female"]
+random_school = ["Johns Hopkins University School of Medicine",
+                 "Stanford School of Medicine",
+                 "Yale School of Medicine",
+                 "Washington University School of Medicine",
+                 "Universit of Pennsylvania Perelman School of Medicine",
+                 "UC San Diego School of Medicine",
+                 "Vanderbilt School of Medicine",
+                 "Duke School of Medicine",
+                 "Michigan Medican School",
+                 "Weill Cornell Medical College"]
+random_dental_school = ["Columbia University College of Dental Medicine",
+                        "University of Pennsylvania School of Dental Medicine",
+                        "Harvard University School of Dental Medicine",
+                        "University of Michigan School of Dentistry"]
+random_address = ["462 1st Avenue, New York, NY",
+                  "51 W 51st St, New York, NY",
+                  "53 W 23rd St, New York, NY",
+                  "5 Pennsylvania Plaza, New York, NY",
+                  "210 E 64th St, New York, NY"]
+
+random_dental_address = ["49 W 23rd St, 12th Floor, New York, NY",
+                         "220 W 26th St, New York, NY",
+                         "241 W 30th St, New York, NY",
+                         "29 W 19th St, New York, NY"]
 
 10.times do |i|
   doctors << Doctor.create!(first_name: doc_first_names[i],
@@ -63,11 +107,29 @@ doctors = []
                             address: addresses[i])
 end
 
+90.times do |i|
+  doctors << Doctor.create!(first_name: random_first_name.shuffle[0],
+                            last_name: random_last_name.shuffle[0],
+                            education: random_school.shuffle[0],
+                            degree: "MD",
+                            gender: random_gender.shuffle[0],
+                            address: random_address.shuffle[0])
+end
+
+10.times do |i|
+  doctors << Doctor.create!(first_name: random_first_name.shuffle[0],
+                            last_name: random_last_name.shuffle[0],
+                            education: random_dental_school.shuffle[0],
+                            degree: ["DMD", "DDS"].shuffle[0],
+                            gender: random_gender.shuffle[0],
+                            address: random_dental_address.shuffle[0])
+end
+
 Appointment.destroy_all
 doctors.each do |doctor|
   start_day = Time.new(2017, 8, 29, 8)
 
-  100.times do
+  300.times do
     Appointment.create!(doctor_id: doctor.id,
                         start_time: start_day.to_datetime)
 
@@ -180,3 +242,18 @@ DoctorSpecialty.create!(specialty_id: specialties[33].id, doctor_id: doctors[7].
 DoctorSpecialty.create!(specialty_id: specialties[16].id, doctor_id: doctors[8].id) # Podiatry
 
 DoctorSpecialty.create!(specialty_id: specialties[7].id, doctor_id: doctors[9].id) # Cancer
+
+doctors[10..49].each do |doctor|
+  DoctorSpecialty.create!(specialty_id: specialty_names[0..3].shuffle,
+                          doctor_id: doctor.id)
+end
+
+doctors[50..99].each do |doctor|
+  DoctorSpecialty.create!(specialty_id: specialty_names[5..33].shuffle,
+                          doctor_id: doctor.id)
+end
+
+doctors[100..109].each do |doctor|
+  DoctorSpecialty.create!(specialty_id: specialty_names[34..38].shuffle,
+                          doctor_id: doctor.id)
+end

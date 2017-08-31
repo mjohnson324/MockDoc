c75578a409c1ea67488a4269bc9db3271f61afde
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-08-31 14:46:06 -0400

    Create actions for receiving single appointments

--- frontend/actions/appointment_actions.js
+++ frontend/actions/appointment_actions.js
@@ -2,6 +2,7 @@ import * as APIUtil from '../util/appointment_api_util';
 
 export const RECEIVE_APPOINTMENTS = 'RECEIVE_APPOINTMENTS';
 export const SCHEDULE_APPOINTMENT = 'SCHEDULE_APPOINTMENT';
+export const RECEIVE_APPOINTMENT = 'RECEIVE_APPOINTMENT';
 
 export const receiveAppointments = appointments => {
   return {
@@ -9,6 +10,13 @@ export const receiveAppointments = appointments => {
     appointments,
   };
 };
+
+export const receiveAppointment = appointment => {
+  return {
+    type: RECEIVE_APPOINTMENT,
+    appointment,
+  };
+}
 
 export const scheduleAppointment = appointment => {
   return {
@@ -20,6 +28,12 @@ export const scheduleAppointment = appointment => {
 export const getAppointments = filters => dispatch => {
   return APIUtil.getApps(filters).then(apps => (
     dispatch(receiveAppointments(apps))
+  ));
+};
+
+export const getAppointment = appointment => dispatch => {
+  return APIUtil.getApp(appointment).then(app => (
+    dispatch(receiveAppointment(app))
   ));
 };
 

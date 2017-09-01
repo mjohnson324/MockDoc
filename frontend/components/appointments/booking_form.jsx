e0cf841a6aa0f6934249ed7b5939f3cdac383b3c
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-01 12:01:39 -0400

    style booking form, add event handler functions

--- frontend/components/appointments/booking_form.jsx
+++ frontend/components/appointments/booking_form.jsx
@@ -4,6 +4,10 @@ import moment from 'moment';
 class BookingForm extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      reason: '',
+    };
   }
 
   componentDidMount() {
@@ -11,43 +15,71 @@ class BookingForm extends React.Component {
     this.props.getAppointment(appId);
   }
 
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const data = {
+      reason: this.state.reason,
+      patient_id: this.props.currentUser.id
+    };
+
+    this.props.updateAppointment(data)
+      .then(() => this.props.history.push('/patient'));
+  }
+
+  // renderErrors() {
+  //   return(
+  //     <ul className="error">
+  //       {this.props.errors.map((error, i) => (
+  //         <li key={`error-${i}`}>
+  //           {error}
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
+
   render() {
     const { appointment, patient } = this.props;
 
     if (appointment) {
       return(
-        <form className="booking-form">
+        <form onSubmit={this.handleSubmit} className="booking-form">
           <h3>Review and Book</h3>
 
-          <label>Name:
-            <div>{`${patient.first_name} ${patient.last_name}`}</div>
-          </label>
+          <label>Name:</label>
+          <div>{`${patient.first_name} ${patient.last_name}`}</div>
           <br/>
 
-          <label>Time:
-            <div>
-              {`${moment(appointment.start_time)
-                .format("dddd, MMMM Do YYYY, h:mm a")}`}
-              </div>
-          </label>
+          <label>Time:</label>
+          <div>
+            {`${moment(appointment.start_time)
+              .format("dddd, MMMM Do YYYY, h:mm a")}`}
+          </div>
           <br/>
 
-          <label>Address:
-            <div>{`${appointment.address}`}</div>
-          </label>
+          <label>Address:</label>
+          <div>{`${appointment.address}`}</div>
           <br/>
 
-          <label>Doctor:
-            <div>`${appointment.doctor_name}`</div>
-          </label>
+          <label>Doctor:</label>
+          <div>{`${appointment.doctor_name}`}</div>
           <br/>
 
-          <label>Reason for visit:
-            <textarea onChange={this.update('body')}></textarea>
-          </label>
+          <label>Reason for visit:</label>
+          <br/>
+          <textarea
+            onChange={this.update('body')}
+            value={this.state.reason}
+            ></textarea>
+          <br/>
 
-
-          <input type="submit"></input>
+          <input type="submit" value="Book Appointment" />
         </form>
       );
     } else {

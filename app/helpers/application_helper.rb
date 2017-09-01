18b0328008baad6d42731dbfc8383547b29e48ec
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-08-31 22:28:23 -0400

    Define helper function for filtering appointments over a timespan

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def day_range(num_days)
+    current_time = Time.now
+
+    days_later = current_time + num_days * 24 * 60 * 60
+    c_yr = days_later.year
+    c_m = days_later.month
+    c_d = days_later.day
+
+    end_time = Time.new(c_yr, c_m, c_d, 23)
+    current_time..end_time
+  end
 end

c5f3179c6a8b4fd5be9c8add560876854fb9f4a3
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-04 14:45:32 -0400

    Update App for Production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,22 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from '../reducers/root_reducer';
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const logger = require('redux-logger');
+  middlewares.push(logger);
+}
+
 import logger from 'redux-logger';
 
-import rootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(...middlewares)
   )
 );
 

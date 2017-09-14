0bd3f2b40237425f86ec3682d6c91bced9eb3c02
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-14 18:08:49 -0400

    Style splash page, and text explaining site's purpose

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -43,24 +43,26 @@ class Header extends React.Component {
   render() {
     if (this.isHomePage()) {
       return(
-        <div>
+        <div className="home">
           <header className="head-home">
-            <h2>MockDoc</h2>
-
+            <h1>MockDoc: You get what you pay for</h1>
             <nav>{this.navLink()}</nav>
           </header>
-
-          <h1 className="home">
-            Need a doctor? Start your search here!
-          </h1>
-
+          <h2>Search for doctors, book appointments, get free healthcare. Simple.</h2>
+          <h3>Healthcare isn't cheap, yet medical errors are among</h3>
+          <h3>the <b>top 10</b> causes of death in the United States.</h3>
+          <p>We at Mockdoc believe patients should get their money's worth.</p>
+          <p>To that end we set a up a site where patients can search for free care from doctors.</p>
+          <p>That's right, we offer <b>FREE</b> healthcare.</p>
+          <p>We can't promise the service is any good (or that it's provided by real doctors),</p>
+          <p>but we guarantee you won't owe us or the physician a dime.</p>
+          <p>After all, if the service is lousy why pay for it?</p>
         </div>
       );
     } else {
       return(
         <header className="head">
-          <Link className="no-extra-blue" to="/"><h1>MockDoc: Home</h1></Link>
-
+          <Link className="no-extra-blue" to="/"><h1>MockDoc</h1></Link>
           <nav>
             {this.navLink()}
           </nav>

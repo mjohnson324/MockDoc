e47f55330810996b23aeec4617d3e66587b922a0
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-14 16:41:03 -0400

    Update imports to session and signup containers

--- frontend/components/session/session_form_container.js
+++ frontend/components/session/session_form_container.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
-import { logIn, clearErrors } from '../../actions/session_actions';
+import { logIn } from '../../actions/session_actions';
+import { clearErrors } from '../../actions/error_actions';
 import SessionForm from './session_form';
 import { withRouter } from 'react-router-dom';
 
--- frontend/components/signup/signup_form_container.js
+++ frontend/components/signup/signup_form_container.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { signUp } from '../../actions/session_actions';
 import { clearErrors } from '../../actions/error_actions';
-import { SignUpForm } from './signup_form';
+import SignUpForm from './signup_form';
 import { withRouter } from 'react-router-dom';
 
 const mapStatetoProps = (state) => {

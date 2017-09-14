d481d18ff705a3b13fb412c1439cf0000598f719
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-14 15:34:05 -0400

    conditionally remove logger method from production database

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -5,12 +5,9 @@ import rootReducer from '../reducers/root_reducer';
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV !== 'production') {
-  const logger = require('redux-logger');
+  const { logger } = require('redux-logger');
   middlewares.push(logger);
 }
-
-import logger from 'redux-logger';
-
 
 const configureStore = (preloadedState = {}) => (
   createStore(

9b56237e6ca31a7d3a13efb29caad7fe9b357154
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-27 02:58:55 -0400

    Fill out review form methods

--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -5,39 +5,55 @@ class ReviewForm extends React.Component {
     super (props);
 
     this.state = {
-
+      overall_rating: 0,
+      wait_time: 0,
+      bedside_manner: 0,
+      body: ''
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
-
+    this.props.clearErrors();
   }
 
-  update() {
-
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
   }
 
   handleSubmit(e) {
-
+    e.preventDefault();
+    const review = this.state;
+    this.props.createReview({review})
+      .then(() => this.props.history.push('/patient'));
   }
 
   renderErrors() {
-
+    return(
+      <ul className="error">
+        {this.props.errors.map((error, i) => (
+          <li key={`error-${i}`}>
+            {error}
+          </li>
+        ))}
+      </ul>
+    );
   }
 
   render() {
     return(
       <form>
-        <h3></h3>
+        <h3>Leave a Review</h3>
         <input></input>
         <br/>
         <input></input>
         <br/>
         <input></input>
         <br/>
-        <input/>
+        <input onSubmit={this.handleSubmit}/>
       </form>
     );
   }

e9560d476f6171e32d2320ed320c0132bf80479e
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-27 02:59:42 -0400

    Import functions to map state and dispatch to props

--- frontend/components/reviews/review_form_container.js
+++ frontend/components/reviews/review_form_container.js
@@ -1,17 +1,20 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-
+import { createReview } from '../../actions/review_actions';
+import { clearErrors } from '../../actions/error_actions';
+import { selectErrors } from '../../reducers/selectors';
 import ReviewForm from './review_form';
 
 const mapStatetoProps = state => {
   return {
-
+    errors: selectErrors(state),
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-
+    createReview: review => dispatch(createReview(review)),
+    clearErrors: () => dispatch(clearErrors()),
   };
 };
 

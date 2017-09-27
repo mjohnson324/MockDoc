058c3042b2a105863c1277116a48d98278ace57b
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-27 00:45:53 -0400

    Make homepage presentation more professional in tone

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -48,15 +48,8 @@ class Header extends React.Component {
             <h1>MockDoc: You get what you pay for</h1>
             <nav>{this.navLink()}</nav>
           </header>
-          <h2>Search for doctors, book appointments, get free healthcare. Simple.</h2>
-          <h3>Healthcare isn't cheap, yet medical errors are among</h3>
-          <h3>the <b>top 10</b> causes of death in the United States.</h3>
-          <p>We at Mockdoc believe patients should get their money's worth.</p>
-          <p>To that end we set a up a site where patients can search for free care from doctors.</p>
-          <p>That's right, we offer <b>FREE</b> healthcare.</p>
-          <p>We can't promise the service is any good (or that it's provided by real doctors),</p>
-          <p>but we guarantee you won't owe us or the physician a dime.</p>
-          <p>After all, if the service is lousy why pay for it?</p>
+          <h2>Search for doctors, book appointments, get free healthcare. Simple.*</h2>
+          <p>*This app is for demo purposes only. Any and all doctors listed are purely fictional.</p>
         </div>
       );
     } else {

0de8f01dfdf1483f4f38f5c5e4cdb6ef09e41812
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-27 11:54:26 -0400

    Sort appointment and review information with helper functions from selectors

--- frontend/components/doctors/doctor_container.js
+++ frontend/components/doctors/doctor_container.js
@@ -1,17 +1,19 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { sortAppointmentsByDoctor } from '../../reducers/selectors';
+import { getAppointments, getReviews } from '../../reducers/selectors';
 
 import { getADoctor } from '../../actions/doctor_actions';
 import Doctor from './doctor';
 
 const mapStatetoProps = (state, ownProps) => {
   let thisDoc = state.doctors[ownProps.match.params.id];
+  const appointmentIds = thisDoc === null ? [] : thisDoc.appointment_ids;
+  const reviewIds = thisDoc === null ? [] : thisDoc.review_ids;
 
   return {
     doctor: thisDoc,
-    appointments: sortAppointmentsByDoctor([thisDoc], state.appointments),
-    reviews: state.reviews,
+    appointments: getAppointments(appointmentIds, state.appointments),
+    reviews: getReviews(reviewIds, state.reviews),
   };
 };
 

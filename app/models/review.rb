b01897814002fa63edc9ace456f644272a5c8278
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-26 22:49:17 -0400

    Validate reviews using custom validations to verify presence of patient and that review is posted after appointment

--- app/models/review.rb
+++ app/models/review.rb
@@ -10,15 +10,29 @@
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
 #  appointment_id :integer          not null
+#  doctor_id      :integer          not null
 #
 
 class Review < ApplicationRecord
-  validates :appointment_id, presence: true, uniqueness: true
+  validates :appointment_id, :doctor_id, presence: true
+  validates :appointment_id, uniqueness: true
   validates :overall_rating,
             :bedside_manner,
             :wait_time, numericality: { only_integer: true,
                                         greater_than_or_equal_to: 1,
                                         less_than_or_equal_to: 5 }
+  validate :patient_is_present
+  after_validation :check_review_time
 
   belongs_to :appointment
+
+  def patient_is_present
+    unless self.appointment.patient_id
+      errors.add(:appointment_id, "Appointment must have a patient")
+    end
+  end
+
+  def check_review_time
+    if self.created_at < self.appointment.start_time - 2.hours
+  end
 end

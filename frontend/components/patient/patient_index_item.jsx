55679201ffdb6ba703f02a8c00a4c3ca5c7b11c5
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-09-28 13:51:42 -0400

    Apply minimal styling to patient profile page

--- frontend/components/patient/patient_index_item.jsx
+++ frontend/components/patient/patient_index_item.jsx
@@ -6,13 +6,13 @@ const PatientIndexItem = (props) => {
   const { appointment, doctor, review, cancel, removeReview } = props;
   return(
     <li>
-      <div>Appointment:
+      <div className="patient-appointment-info">Appointment:
         <div>Reason: {`${appointment.reason}`}</div>
         <div>Time: {`${moment(appointment.start_time)
             .format("dddd, MMMM Do YYYY, h:mm a")}`}</div>
         <div>Address: {`${doctor.address}`}</div>
         <div>Doctor: {`${doctor.first_name} ${doctor.last_name}`}</div>
-        <button onClick={cancel}>Cancel Appointment</button>
+        {cancelButton(appointment, cancel)}
       </div>
       {reviewPortion(review, removeReview, appointment)}
     </li>
@@ -22,7 +22,7 @@ const PatientIndexItem = (props) => {
 const reviewPortion = (review, removeReview, appointment) => {
   if (review !== undefined) {
     return(
-      <div>Review:
+      <div className="patient-appointment-info">Review:
         <div>Overall: {`${review.overall_rating}`}</div>
         <div>Bedside Manner: {`${review.bedside_manner}`}</div>
         <div>Wait-Time: {`${review.wait_time}`}</div>
@@ -37,6 +37,13 @@ const reviewPortion = (review, removeReview, appointment) => {
 const reviewBody = body => {
   if (body !== null) {
     return <p>Body: {`${body}`}</p>;
+  }
+};
+
+const cancelButton = (appointment, callback) => {
+  const startTime = moment(appointment.start_time);
+  if (startTime._d > new Date()) {
+    return <button onClick={callback}>Cancel Appointment</button>;
   }
 };
 

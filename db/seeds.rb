ade87d274ae7366279c0d8cc9d232829e3c08727
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2017-10-08 12:04:54 -0400

    Make seeding work consistently

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,11 +37,11 @@ random_dental_address = ["49 W 23rd St, 12th Floor, New York, NY", "220 W 26th S
   doctors << Doctor.create!(first_name: doc_first_names[i], last_name: doc_last_names[i], education: education[i], degree: degrees[i], gender: genders[i], address: addresses[i])
 end
 50.times do |i|
-  sleep(2) if i % 3 == 0
+  sleep(3)
   doctors << Doctor.create!(first_name: random_first_name.shuffle[0], last_name: random_last_name.shuffle[0], education: random_school.shuffle[0], degree: "MD", gender: random_gender.shuffle[0], address: random_address.shuffle[0])
 end
 10.times do |i|
-  sleep(2) if i % 3 == 0
+  sleep(3)
   doctors << Doctor.create!(first_name: random_first_name.shuffle[0], last_name: random_last_name.shuffle[0], education: random_dental_school.shuffle[0], degree: ["DMD", "DDS"].shuffle[0], gender: random_gender.shuffle[0], address: random_dental_address.shuffle[0])
 end
 

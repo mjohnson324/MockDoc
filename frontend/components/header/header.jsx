f5728a2b6ac813203f6616d7871beedcc39421f1
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-07-29 19:42:47 -0400

    Refactor header component, reducing html and cleaning up conditional rendering

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,67 +1,51 @@
 import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   constructor(props){
     super(props);
   }
 
-  navLink() {
-    if (this.props.currentUser) {
-      if (this.props.location.pathname === '/patient') {
-        return(
-          <nav>
-            <button onClick={this.props.logOut}>Sign Out</button>
-          </nav>
-        );
-      } else {
-        return(
-          <nav>
-            <Link className="header-profile" to="/patient">Profile</Link>
-            <button onClick={this.props.logOut}>Sign Out</button>
-          </nav>
-        );
-      }
+  navLinks() {
+    const { currentUser, location, logOut } = this.props;
+    if (currentUser !== null) { 
+      return(
+        <React.Fragment>
+          {this.isNotProfile(location.pathname)}
+          <button onClick={logOut}>Sign Out</button>
+        </React.Fragment>
+      );
     } else {
-      if (this.props.location.pathname === '/signin' ||
-          this.props.location.pathname === '/createuser') {
-            return <div></div>;
-          } else {
-            return <Link to="/signin">Sign In</Link>;
-          }
+      return( 
+        <React.Fragment>
+          <Link to="/signin">Sign In</Link>
+          <Link to="/createuser">Sign Up</Link>
+        </React.Fragment>
+      );
+    }
+  }
+
+  isNotProfile(path) {
+    if (path !== '/patient') {
+      return <Link className="header-profile" to="/patient">Profile</Link>;
     }
   }
 
   isHomePage() {
     if (this.props.location.pathname === "/") {
-      return true;
+      return <img id="logo" alt={"logo"} />;
     } else {
-      return false;
+      return <Link className="no-extra-blue" to="/"><h1>MockDoc</h1></Link>;
     }
   }
 
   render() {
-    if (this.isHomePage()) {
-      return(
-        <div className="home">
-          <header className="head-home">
-            <h1>MockDoc</h1>
-            <nav>{this.navLink()}</nav>
-          </header>
-          <h2>Search for doctors, book appointments, get free healthcare. Simple.*</h2>
-          <p className="home-disclaimer">*This app is for demo purposes only. Any and all doctors listed are purely fictional.</p>
-        </div>
-      );
-    } else {
-      return(
-        <header className="head">
-          <Link className="no-extra-blue" to="/"><h1>MockDoc</h1></Link>
-          <nav>
-            {this.navLink()}
-          </nav>
-        </header>
-      );
-    }
+    return(
+      <header className="head-home">
+        {this.isHomePage()}
+        <nav>{this.navLinks()}</nav>
+      </header>
+    );
   }
 }
 

fa2aa5f255245921622f889a08683165e588a822
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-07-29 22:42:16 -0400

    Refactor: replace text entry with options selection to improve UX

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -32,7 +32,7 @@ class Search extends React.Component {
 
   checkParams(filter) {
     if (filter.specialty === '') {
-      filter.specialty = "primary care physician";
+      filter.specialty = "primary care";
     }
 
     if (filter.address === '') {
@@ -67,17 +67,34 @@ class Search extends React.Component {
 
     return(
       <form className={searchClass} onSubmit={this.handleSubmit}>
-        <input type="text"
-          placeholder="Specialist"
-          value={this.state.specialty}
-          onChange={this.update('specialty')} />
-
-        <input type="text"
-          placeholder="Address"
+        <label htmlFor="specialty-select">Specialty:</label>
+        <select id="specialty-select" 
+        onChange={this.update('specialty')}
+        defaultValue="none">
+          <option value="none" disabled>Please Choose</option>
+          <option value="primary care">Primary Care</option>
+          <option value="pediatrics">Pediatrics</option>
+          <option value="chiropractor">Chiropractor</option>
+          <option value="gynecology">Gynecology</option>
+          <option value="oncology">Oncology</option>
+          <option value="cardiology">Cardiology</option>
+          <option value="dermatology">Dermatology</option>
+          <option value="gastroenterology">Digestion</option>
+          <option value="neurology">Neurology</option>
+          <option value="radiology">Radiology</option>
+          <option value="opthalmology">Eye Doctor</option>
+          <option value="surgery">Surgery</option>
+          <option value="plastic surgery">Plastic Surgery</option>
+          <option value="psychiatry">Mental Health</option>
+          <option value="dentist">Dentist</option>
+        </select>
+        <label htmlFor="address">Address:</label>
+        <input id="address" 
+          type="text"
           value={this.state.address}
           onChange={this.update('address')} />
 
-        <input type="submit" value="Search"/>
+        <input type="submit" value="&#x1F50D;"/>
       </form>
     );
   }

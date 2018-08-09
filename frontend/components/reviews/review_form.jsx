9ed0cd9971dc5feb17f2a659c1b599a5f69c2336
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-08-08 21:31:30 -0400

    Refactor: change how review data is integrated and uploaded in ReviewForm

--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -1,35 +1,28 @@
+/* eslint prefer-object-spread: "off" */
 import React from 'react';
+import { Errors } from '../header/shared_components';
 
 class ReviewForm extends React.Component {
   constructor(props) {
     super (props);
-      this.state = {
-        overall_rating: '',
-        wait_time: '',
-        bedside_manner: '',
-        body: '',
-      };
-
+    const { review } = this.props;
+    this.state = {
+      overall_rating: review.overall_rating,
+      wait_time: review.wait_time,
+      bedside_manner: review.bedside_manner,
+      body: review.body === null ? '' : review.body,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount() {
+  componentWillUnmount() {
     this.props.clearErrors();
+  }
+
+  componentDidMount() {
     const appointmentId = window.location.href.split('-')[1];
     this.props.getAppointment(appointmentId);
   }
-
-  // componentWillReceiveProps(nextProps) {
-  //   const review = nextProps.review;
-  //   if (review !== undefined) {
-  //     this.setState({
-  //       overall_rating: review.overall_rating,
-  //       wait_time: review.wait_time,
-  //       bedside_manner: review.bedside_manner,
-  //       body: (review.body === null ? '' : review.body),
-  //     });
-  //   }
-  // }
 
   update(field) {
     return e => this.setState({
@@ -40,69 +33,53 @@ class ReviewForm extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     const review = this.state;
+    const id = this.props.review.id;
     review["appointment_id"] = this.props.appointment.id;
-    if (this.props.review !== undefined) {
-      review["id"] = this.props.review.id;
+    if (id !== undefined) {
+      review["id"] = id;
       this.props.changeReview(review)
-      .then(() => this.props.history.push('/patient'));
+        .then(() => this.props.history.push('/patient'));
     } else {
       this.props.createReview(review)
-      .then(() => this.props.history.push('/patient'));
+        .then(() => this.props.history.push('/patient'));
     }
-  }
-
-  renderErrors() {
-    return(
-      <ul className="error">
-        {this.props.errors.map((error, i) => (
-          <li key={`error-${i}`}>
-            {error}
-          </li>
-        ))}
-      </ul>
-    );
   }
 
   render() {
-    const { appointment } = this.props;
-    if (appointment) {
-      return(
-        <form onSubmit={this.handleSubmit} className="review-form">
-          <h3>Leave a Review</h3>
-          <label>Overall Rating:</label>
-          <input type="text"
-            placeholder="type an integer, 1 to 5"
-            value={this.state.overall_rating}
-            onChange={this.update('overall_rating')}
-            />
-          <br/>
-          <label>Bedside Manner:</label>
-          <input type="text"
-            placeholder="type an integer, 1 to 5"
-            value={this.state.bedside_manner}
-            onChange={this.update('bedside_manner')}
-            />
-          <br/>
-          <label>Wait Time:</label>
-          <input type="text"
-            placeholder="type an integer, 1 to 5"
-            value={this.state.wait_time}
-            onChange={this.update('wait_time')}
+    return(
+      <form onSubmit={this.handleSubmit} className="review-form">
+        <h3>Leave a Review</h3>
+        <label>Overall Rating:</label>
+        <input type="text"
+          placeholder="type an integer, 1 to 5"
+          value={this.state.overall_rating}
+          onChange={this.update('overall_rating')}
           />
-          <br/>
-          <label>Leave a Detailed Review Here (optional):</label>
-          <textarea
-            onChange={this.update('body')}
-            value={this.state.body}
-            ></textarea>
-          <br/>
-          {this.renderErrors()}
-          <input type="submit" value="Submit Review"/>
-        </form>
-      );
-    } else {
-      return <section />;
-    }
+        <br/>
+        <label>Bedside Manner:</label>
+        <input type="text"
+          placeholder="type an integer, 1 to 5"
+          value={this.state.bedside_manner}
+          onChange={this.update('bedside_manner')}
+          />
+        <br/>
+        <label>Wait Time:</label>
+        <input type="text"
+          placeholder="type an integer, 1 to 5"
+          value={this.state.wait_time}
+          onChange={this.update('wait_time')}
+        />
+        <br/>
+        <label>Leave a Detailed Review Here (optional):</label>
+        <textarea
+          onChange={this.update('body')}
+          value={this.state.body}
+          ></textarea>
+        <br/>
+        <Errors errors={this.props.errors} />
+        <input type="submit" value="Submit Review"/>
+      </form>
+    );
   }
 }
 

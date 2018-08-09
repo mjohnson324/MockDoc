c63f8e26b92ce1a745dd69a5eb245da9e0a50c6a
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-08-08 21:29:22 -0400

    Add loading status to search results

--- frontend/components/search/search_index.jsx
+++ frontend/components/search/search_index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { getDayRange } from '../../util/appointment_util';
-import { sortAppointmentsByDoctor } from '../../reducers/selectors';
 import SearchIndexItem from './search_index_item';
 import DoctorsMap from '../map/doctors_map';
+import { Loading, NoResultsFound } from '../header/shared_components';
 
 class SearchIndex extends React.Component {
   constructor(props) {
@@ -15,51 +15,41 @@ class SearchIndex extends React.Component {
     };
   }
 
-  componentDidMount() {
-    const queryString = "?".concat(window.location.href.split('?')[1]);
-    const searchParams = new URLSearchParams(queryString);
-
-    const filters = {
-      specialty: searchParams.get('specialty'),
-      address: searchParams.get('address'),
-    };
-
-    this.props.getDoctors(filters);
-  }
-
   render() {
-    const { doctors, appointments } = this.props;
-    const docSortedAppointments = sortAppointmentsByDoctor(
-      doctors, appointments
-    );
-    const { today, tomorrow, dayAfter }  = this.state;
-    return(
-      <div className="search-master">
-        <section className="search-results">
-          <div className="appointment-scroll">
-            <button>{'\u{1f844}'}</button>
-            <div>{`${today}`.slice(0, 10)}</div>
-            <div>{`${tomorrow}`.slice(0, 10)}</div>
-            <div>{`${dayAfter}`.slice(0, 10)}</div>
-            <button className="right-button">{'\u{1f846}'}</button>
-          </div>
-
-          <ul>
-            {doctors.map(doctor => (
-              <SearchIndexItem
-                key={doctor.id}
-                doc={doctor}
-                apps={docSortedAppointments[doctor.id]}
-                dates={[today, tomorrow, dayAfter]} />))}
-          </ul>
-        </section>
-
-        <DoctorsMap
-          doctors={doctors}
-          address={this.props.filters.address}
-        />
-      </div>
-    );
+    const { status } = this.props;
+    if (status === "loading") {
+      return <Loading />;
+    } else if (status === "failure"){
+        return <NoResultsFound />;
+    } else {
+      const { appointments, address, doctors } = this.props;
+      const { today, tomorrow, dayAfter }  = this.state;
+      return(
+        <div className="search-master">
+          <section className="search-results">
+            <div className="appointment-scroll">
+              <button>{'\u{1f844}'}</button>
+              <div>{`${today}`.slice(0, 10)}</div>
+              <div>{`${tomorrow}`.slice(0, 10)}</div>
+              <div>{`${dayAfter}`.slice(0, 10)}</div>
+              <button className="right-button">{'\u{1f846}'}</button>
+            </div>       
+            <ul>
+              {doctors.map(doctor => (
+                <SearchIndexItem
+                  key={doctor.id}
+                  doc={doctor}
+                  apps={appointments[doctor.id]}
+                  dates={[today, tomorrow, dayAfter]} />))}
+            </ul>
+          </section>
+          <DoctorsMap
+            doctors={doctors}
+            address={address}
+          />
+        </div>
+      );
+    }
   }
 }
 

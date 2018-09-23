0b31fa5b6912e9cca3785f0888ebd61807551194
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-09-23 02:18:08 -0400

    Edit HTML text

--- frontend/ProjectRoot/components/landing_page_expo.jsx
+++ frontend/ProjectRoot/components/landing_page_expo.jsx
@@ -9,7 +9,7 @@ const HomeText = () => {
       &nbsp;&nbsp;&nbsp;&nbsp;On this hypothetical platform people can book appointmentments for free care from bad doctors. This site was built around the idea of patients seeking free care in the same way many consumers attempt to access media on the internet.
     </p>
     <p>
-      &nbsp;&nbsp;&nbsp;&nbsp;Some might ask why anyone would request bad care, or point out that patients normally don't shop for healthcare this way. I would counter with a simple question: if you need care but can&apos;t influence quality and the service will be lousy no matter what, why pay for it? Hence the idea for this site&apos;s theme and its slogan:       
+      &nbsp;&nbsp;&nbsp;&nbsp;Some might ask why anyone would request bad care, or point out that patients normally don&apos;t shop for healthcare this way. I would counter with a simple question: if you need care but can&apos;t influence quality and the service will be lousy no matter what, why pay for it? Hence the idea for this site&apos;s theme and its slogan:       
     </p>
     <p className="slogan">
       MockDoc: get what you pay for in healthcare.

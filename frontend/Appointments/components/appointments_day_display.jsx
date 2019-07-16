990833b0a52b37d22e3b5ec253ed14d225fc31eb
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2019-07-15 23:14:13 -0400

    Fixed display problem with empty lists

--- frontend/Appointments/components/appointments_day_display.jsx
+++ frontend/Appointments/components/appointments_day_display.jsx
@@ -3,14 +3,16 @@ import moment from 'moment';
 import { Link } from 'react-router-dom';
 
 export const AppointmentsDayDisplay = ({ day }) => {
+  let displayResult = <div>All Booked</div>;
+  if (day.length > 0) {
+    displayResult = day.map(app => (
+      <li key={app.id}>
+        <Link to={`/booking/${app.id}`}>
+          {`${moment(app.start_time).format("h:mm a")}`}
+        </Link>
+      </li>));
+  }
   return(
-    <ul>
-      {day.map(app => (
-        <li key={app.id}>
-          <Link to={`/booking/${app.id}`}>
-            {`${moment(app.start_time).format("h:mm a")}`}
-          </Link>
-        </li>))}
-    </ul>
+    <ul>{displayResult}</ul>
   );
 };
